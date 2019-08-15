849bd518a8333c05bec9c8bd28e449b67a2daf6c
OliverSjolind <oliversjolind@gmail.com>
2019-08-15 13:55:26 +0300

    made the website look better

--- Hello Name/script.js
+++ Hello Name/script.js
@@ -1,3 +1,17 @@
-function nameAlert() {
-    alert('Hello ' + document.getElementById("name").value + '!');
+// Lösning 1
+// function nameAlert() {
+//     alert('Hello ' + document.getElementById("name").value + '!');
+// }
+
+let input = document.getElementById('name');
+let button = document.getElementById('button');
+
+button.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+    if (input.value == '') {
+        alert('Please enter your name!')
+    } else {
+        alert('Hello ' + input.value + '!');
+    }
 }
