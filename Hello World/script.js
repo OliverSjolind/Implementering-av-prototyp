c0a187b2a930caeadb53db971976777276a274a0
OliverSjolind <oliversjolind@gmail.com>
2019-08-20 13:01:12 +0300

    made Hello World

---
+++ Hello World/script.js
@@ -0,0 +1,8 @@
+let myButton = document.getElementById('button')
+let myText = 'Hello World!'
+
+myButton.addEventListener('click', onButtonClicked)
+
+function onButtonClicked() {
+    alert(myText)
+}
