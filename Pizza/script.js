51d0787b8bda5b2b67201c38fb932fd2a6dade6d
OliverSjolind <oliversjolind@gmail.com>
2019-08-23 12:49:01 +0300

    made the pizza shop javascript work

--- Pizza/script.js
+++ Pizza/script.js
@@ -3,15 +3,45 @@ let pizzaSize = document.getElementsByName('pizzaSize')
 let pizzaTopping1 = document.getElementById('pizzaTopping1')
 let pizzaTopping2 = document.getElementById('pizzaTopping2')
 let mediumPizza = document.getElementById('medium')
-let priceDisplay = document.getElementById('priceDisplay'),
-    price = 0;
+let largePizza = document.getElementById('large')
+let priceDisplay = document.getElementById('priceDisplay')
+let pepperoni = document.getElementById('pepperoni')
+let ham = document.getElementById('ham')
+let mozzarella = document.getElementById('mozzarella')
+let mushroom = document.getElementById('mushroom')
+let bacon = document.getElementById('bacon')
 
+let toppings = document.getElementById('toppings')
+let price = 0
 
 pizzaForm.addEventListener('change', pizzaPrice)
 
+
+
 function pizzaPrice() {
-    if (mediumPizza.checked == true) {
+    price = 0
+    if (mediumPizza.checked === true) {
         price += 7
+        pepperoni.disabled = false;
     }
-    priceDisplay.innerHTML = 'Price: ' + price
+    if (largePizza.checked === true) {
+        price += 9
+    }
+    if (pepperoni.checked === true) {
+        price += 1.99
+    }
+    if (ham.checked === true) {
+        price += 1.99
+    }
+    if (mozzarella.checked === true) {
+        price += 0.99
+    }
+    if (mushroom.checked === true) {
+        price += 1.5
+    }
+    if (bacon.checked === true) {
+        price += 1.99
+    }
+    price = Math.round(price * 100) / 100
+    priceDisplay.innerHTML = 'Price: ' + price;
 }
